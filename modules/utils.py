395e62bb686f0a7fd5ecfacde2f51c2e03009245
Kaique Silva <kaique.developer@gmail.com>
2012-01-31 17:45:36 -0200

    class for get itens in table ok

--- modules/utils.py
+++ modules/utils.py
@@ -1,28 +1,42 @@
 # Class table tools
 class Table( object ):
     # Define db and tablename
-	def __init__( self, db, tablename='' ):
+	def __init__(self, db, tablename=''):
 		self.db = db
 		self.tablename = tablename
 	
-	# Get informations of table by fields or all informations
-	def get( self, fields={} ):
+    # Get informations of table by fields or all informations
+	def get(self, fields={}, order=''):
 		try:
 			db = self.db
 			table = db.get(self.tablename)
-			
-			return db().select(table.ALL)
-			
+	
+			if fields:
+				query = ( table )
+				
+				for field in fields:
+					query = query and ( table[field] == fields[field] )
+	
+				return db( query ).select()
+			else:
+				return db().select( table.ALL )
 		except Exception:
 			return False
 	
 	# Get counter of all rows or rows by specified fields
-	def score( self, fields={} ):
+	def score(self, fields={}):
 		try:
 			db = self.db
 			table = db.get(self.tablename)
-			
-			return db(table).count()
-			
+
+			if fields:
+				query = ( table )
+
+				for field in fields:
+					query = query and ( table[field] == fields[field] )
+
+				return db( query ).count()
+			else:
+				return db( table ).count()
 		except Exception:
 			return False
